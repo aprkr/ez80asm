8d1ba9d82b0d249f7194e9151946ecefcca660fc
Alex P <2703parker@gmail.com>
2020-10-20 14:12:13 -0500

    Remove file icons and move hover into separate js file

--- out/extension.js
+++ out/extension.js
@@ -1,35 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// exports.deactivate = exports.activate = void 0;
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
-const hexRegex = /^(\$|0x)([0-9a-fA-F]+)$/;
-const binaryRegex = /^%([01]+)$/;
-const integerRegex = /^[0-9]+d?$/;
+const hover = require("./hover");
 
 function activate(context) {
-    context.subscriptions.push(vscode.languages.registerHoverProvider('ez80-asm', 
-        {provideHover(document, position, token) {
-            const range = document.getWordRangeAtPosition(position, /((\$|0x)[A-Fa-f0-9]+\b)|(%[01]+\b)|([0-9]+d?\b)|([.#]?[A-Za-z_]\w*(\\@|:*))/g);
-            if (range) {
-                const text = document.getText(range);
-                let numberValue = undefined;
-                if (hexRegex.test(text)) {
-                    numberValue = parseInt(hexRegex.exec(text)[2], 16);
-                } else if (binaryRegex.test(text)) {
-                    numberValue = parseInt(binaryRegex.exec(text)[1], 2);
-                } else if (integerRegex.test(text)) {
-                    numberValue = parseInt(text);
-                }
-                if (numberValue !== undefined) {
-                    return new vscode.Hover(`\`${numberValue}\`\n\n\`\$${numberValue.toString(16)}\`\n\n\`%${numberValue.toString(2)}\``, range);
-                } else {
-                    //return new vscode.Hover("LOLOLOL", range);
-                }
-            }
-        }}
-    ))
+    context.subscriptions.push(hover)
 }
 exports.activate = activate;
 // this method is called when your extension is deactivated
---
+++ out/hover.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const vscode = require("vscode");
+const hexRegex = /^(\$|0x)([0-9a-fA-F]+)$/;
+const binaryRegex = /^%([01]+)$/;
+const integerRegex = /^[0-9]+d?$/;
+
+vscode.languages.registerHoverProvider('ez80-asm', 
+        {provideHover(document, position, token) {
+            const range = document.getWordRangeAtPosition(position, /((\$|0x)[A-Fa-f0-9]+\b)|(%[01]+\b)|([0-9]+d?\b)|([.#]?[A-Za-z_]\w*(\\@|:*))/g);
+            if (range) {
+                const text = document.getText(range);
+                let numberValue = undefined;
+                if (hexRegex.test(text)) {
+                    numberValue = parseInt(hexRegex.exec(text)[2], 16); // Group 2 of regex is actual digits
+                } else if (binaryRegex.test(text)) {
+                    numberValue = parseInt(binaryRegex.exec(text)[1], 2); // Group 1, the only group, is actual digits
+                } else if (integerRegex.test(text)) {
+                    numberValue = parseInt(text);
+                }
+                if (numberValue !== undefined) {
+                    return new vscode.Hover(`\`${numberValue}\`\n\n\`\$${numberValue.toString(16)}\`\n\n\`%${numberValue.toString(2)}\``, range);
+                } else {
+                    // return new vscode.Hover("LOLOLOL", range);
+                }
+            }
+        }}
+    )
