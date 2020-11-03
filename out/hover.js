72b2fdc46ba3537852c0d5f181a6b5ce02b1ad71
Alex P <2703parker@gmail.com>
2020-11-02 23:14:39 -0600

    Improve hex regex

--- out/hover.js
+++ out/hover.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const vscode = require("vscode");
 const hexRegex = /^(\$|0x)([0-9a-fA-F]+)$/;
+const hexRegex2 = /^([0-9a-fA-F]+)h$/;
 const binaryRegex = /^%([01]+)$/;
 const integerRegex = /^[0-9]+d?$/;
 
@@ -10,7 +11,7 @@ class ASMHoverProvider {
         this.symbolDocumenter = symbolDocumenter;
     }
     provideHover(document, position, token) {
-        const range = document.getWordRangeAtPosition(position, /((\$|0x)[A-Fa-f0-9]+\b)|(%[01]+\b)|([0-9]+d?\b)|([.#]?[A-Za-z_]\w*(\\@|:*))/g);
+        const range = document.getWordRangeAtPosition(position, /((\$|0x)[A-Fa-f0-9]+\b)|(%[01]+\b)|([0-9]+d?\b)|([.#]?[A-Za-z_]\w*(\\@|:*))|([A-Fa-f0-9]+h\b)/g);
         if (range) {
             const text = document.getText(range);
             const symbol = this.symbolDocumenter.symbol(text, document);
@@ -20,6 +21,8 @@ class ASMHoverProvider {
             }
             if (hexRegex.test(text)) {
                 numberValue = parseInt(hexRegex.exec(text)[2], 16); // Group 2 of regex is actual digits
+            } else if (hexRegex2.test(text)) {
+                numberValue = parseInt(hexRegex2.exec(text)[1], 16); // Group 1, the only group, is actual digits
             } else if (binaryRegex.test(text)) {
                 numberValue = parseInt(binaryRegex.exec(text)[1], 2); // Group 1, the only group, is actual digits
             } else if (integerRegex.test(text)) {
